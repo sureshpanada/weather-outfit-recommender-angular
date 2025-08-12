3ad3547a51731b96acd0ec1f4f43a6f6bd74e601
Suresh Panada <suresh.panada@Kongsbergdigital.com>
2025-08-12 19:41:33 +0530

    Added API key to env , and added api url to services

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { MOCK_WEATHER } from '../mock/mock-weather';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { CitySuggestion } from '../interfaces/city-suggestion.interface';
+import { WeatherData } from '../interfaces/weather-data.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  
-  getWeather(city: string): Observable<any> {
-    const weather = MOCK_WEATHER.find(
-      w => w.city.toLowerCase() === city.toLowerCase()
+  private geoUrl = 'https://api.openweathermap.org/geo/1.0/direct';
+  private weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) {}
+
+  getCitySuggestions(query: string, limit: number = 5): Observable<CitySuggestion[]> {
+    const url = `${this.geoUrl}?q=${encodeURIComponent(query)}&limit=${limit}&appid=${environment.openWeatherApiKey}`;
+    return this.http.get<CitySuggestion[]>(url).pipe(
+      catchError(err => throwError(() => err))
     );
-    return of(weather || null);
   }
 
-  getAllCities(): string[] {
-    return MOCK_WEATHER.map(w => w.city);
+  getWeatherByCoords(lat: number, lon: number): Observable<WeatherData> {
+    const url = `${this.weatherUrl}?lat=${lat}&lon=${lon}&appid=${environment.openWeatherApiKey}`;
+    return this.http.get<WeatherData>(url).pipe(
+      catchError(err => throwError(() => err))
+    );
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  openWeatherApiKey: 'YOUR_API_KEY_HERE'
+};
